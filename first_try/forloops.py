01934a0bf3b034a30ff6656b44b5d469ff6995b2
amazing814 <xdhuzheng01085124@gmail.com>
2016-07-08 19:34:00 +0200

    add a try file

---
+++ first_try/forloops.py
@@ -0,0 +1,12 @@
+'''
+Created on 2016年1月29日
+
+@author: huzheng
+'''
+
+if __name__ == '__main__':
+    yourname = input('plz input your name: ')
+    if yourname == 'huzheng':
+        print('welcome back, master')
+    else:
+        print('{0}, plz go, it is not your place.'.format(yourname))
---
+++ first_try/hello_world.py
@@ -0,0 +1,9 @@
+'''
+Created on 2016年1月29日
+
+@author: huzheng
+'''
+import os
+if __name__ == '__main__':
+    print('hello python with PyDev')
+    
